d6de66acda23f602d8f087169c33842a7e345603
Omar Erminy <omrsin@gmail.com>
2017-04-18 16:34:55 +0200

    Adds some examples to demonstrate the behavior of single actions

---
+++ src/examples/de/tudarmstadt/thesis/symspark/examples/java/filter/SingleIntegerFilterExample.java
@@ -0,0 +1,39 @@
+package de.tudarmstadt.thesis.symspark.examples.java.filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+/**
+ * Example with a single filter transformation implemented
+ * as an anonymous function.
+ * Expected analysis result [-1000000, 3]
+ * Expected execution result [3, 3, 4]
+ * @author Omar Erminy (omar.erminy.ugueto@gmail.com)
+ *
+ */
+public class SingleIntegerFilterExample {
+	public static void main(String[] args) {
+		System.out.println("Spark Java Single Integer Filter Test");
+		
+		SparkConf conf = new SparkConf()
+				.setAppName("JavaNumbers")
+				.setMaster("local");
+		
+		JavaSparkContext spark = new JavaSparkContext(conf);
+		
+		List<Integer> numberList = Arrays.asList(1,2,3,3,4);
+		JavaRDD<Integer> numbers = spark.parallelize(numberList);
+		
+		numbers.filter(new Function<Integer, Boolean>() {			
+			@Override
+			public Boolean call(Integer v1) throws Exception {				
+				return v1 > 2;
+			}
+		});
+	}
+}
---
+++ src/examples/de/tudarmstadt/thesis/symspark/examples/java/map/SingleIntegerMapExample.java
@@ -0,0 +1,40 @@
+package de.tudarmstadt.thesis.symspark.examples.java.map;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+/**
+ * Example with a single map transformation implemented
+ * as an anonymous function.
+ * Expected analysis result [-1000000, 6]
+ * Expected execution result [3, 6, 9, 9, 12]
+ * @author Omar Erminy (omar.erminy.ugueto@gmail.com)
+ *
+ */
+public class SingleIntegerMapExample {
+	public static void main(String[] args) {
+		System.out.println("Spark Java Single Integer Map Test");
+		
+		SparkConf conf = new SparkConf()
+				.setAppName("JavaNumbers")
+				.setMaster("local");
+		
+		JavaSparkContext spark = new JavaSparkContext(conf);
+		
+		List<Integer> numberList = Arrays.asList(1,2,3,3,4);
+		JavaRDD<Integer> numbers = spark.parallelize(numberList);
+		
+		numbers.map(new Function<Integer, Integer>() {
+			@Override
+			public Integer call(Integer v1) throws Exception {
+				if(v1 > 5) return v1*2;
+				else return v1*3;
+			}			
+		});
+	}
+}
